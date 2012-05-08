b34eea4b29348d010a33e7638d69704e7d4167f4
Jason Kim <jason@512k.org>
2012-05-08 00:25:09 -0700

    Conform whitespace.

--- sprite.lua
+++ sprite.lua
@@ -1,11 +1,11 @@
 Gob = {
-	x = 0,
-	y = 0,
+    x = 0,
+    y = 0,
     angle = 0,
-	time = 0.0,
-	fps = 10,
-	sprite = nil,
-	currentFrame = 1,
+    time = 0.0,
+    fps = 10,
+    sprite = nil,
+    currentFrame = 1,
 }
 
 Sprite = {
@@ -26,8 +26,8 @@ function Gob:new(data)
             o[k] = v
         end
     end
-	
-	return o
+
+    return o
 end
 
 function Sprite:new(imageName, data)
@@ -63,7 +63,7 @@ function Sprite:newAnim(imageName, numFrames, data)
     local x = 0
     for i = 1, numFrames do
         table.insert(s.anim.quads,
-            love.graphics.newQuad(x, 0, frameWidth, height, width, height))
+        love.graphics.newQuad(x, 0, frameWidth, height, width, height))
         x = x + frameWidth
     end
 
@@ -71,46 +71,46 @@ function Sprite:newAnim(imageName, numFrames, data)
 end
 
 function Gob:draw()
-	local sprite = self.sprite
+    local sprite = self.sprite
     love.graphics.setColor(255, 255, 255, sprite.opacity * 255)
     if (sprite.anim) then
         love.graphics.drawq(
-            sprite.image,
-            sprite.anim.quads[self.currentFrame],
-            self.x, self.y, -self.angle,
-            sprite.scale.x, sprite.scale.y, sprite.center.x, sprite.center.y)
+        sprite.image,
+        sprite.anim.quads[self.currentFrame],
+        self.x, self.y, -self.angle,
+        sprite.scale.x, sprite.scale.y, sprite.center.x, sprite.center.y)
     else
         love.graphics.draw(
-            sprite.image,
-            self.x, self.y, -self.angle,
-            sprite.scale.x, sprite.scale.y, sprite.center.x, sprite.center.y)
+        sprite.image,
+        self.x, self.y, -self.angle,
+        sprite.scale.x, sprite.scale.y, sprite.center.x, sprite.center.y)
     end
 end
 
 function Gob:update(dt)
-	for k,v in ipairs(getmetatable(self)) do
-		print(k .. ": " .. v)
-	end
-	
+    for k,v in ipairs(getmetatable(self)) do
+        print(k .. ": " .. v)
+    end
+
     self.time = self.time + dt
     self.currentFrame = (math.floor(self.time * self.fps) % self.sprite.anim.count) + 1
 end
 
 function Gob:hitGob(obj)
-	return rectsIntersect(
-				self:getWorldSpaceBbox(),
-				obj:getWorldSpaceBbox())
+    return rectsIntersect(
+    self:getWorldSpaceBbox(),
+    obj:getWorldSpaceBbox())
 end
 
 function Gob:getWorldSpaceBbox()
-	local sprite = self.sprite
-	local ul_x = self.x + sprite.hitbox.x - sprite.center.x
-	local ul_y = self.y + sprite.hitbox.y - sprite.center.y
-	local br_x = ul_x + sprite.hitbox.w
-	local br_y = ul_y + sprite.hitbox.h
-	return { ul_x = ul_x, ul_y = ul_y, br_x = br_x, br_y = br_y }
+    local sprite = self.sprite
+    local ul_x = self.x + sprite.hitbox.x - sprite.center.x
+    local ul_y = self.y + sprite.hitbox.y - sprite.center.y
+    local br_x = ul_x + sprite.hitbox.w
+    local br_y = ul_y + sprite.hitbox.h
+    return { ul_x = ul_x, ul_y = ul_y, br_x = br_x, br_y = br_y }
 end
 
 function rectsIntersect(rect1, rect2)
-	return not (rect1.ul_x > rect2.br_x or rect1.br_x < rect2.ul_x or rect1.ul_y > rect2.br_y or rect1.br_y < rect2.ul_y)
+    return not (rect1.ul_x > rect2.br_x or rect1.br_x < rect2.ul_x or rect1.ul_y > rect2.br_y or rect1.br_y < rect2.ul_y)
 end
